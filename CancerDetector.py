1b31ee2d8c854805850431f1d9a5a95f24134d16
miolane <miolane@users.noreply.github.com>
2019-02-26 10:10:14 +0800

    Add demo

--- CancerDetector.py
+++ CancerDetector.py
@@ -2,13 +2,12 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2 as cv
-from PIL import Image
 from model import Model
 
 CKPT_PATH = "model\\model.ckpt"
 
-def detector(path):
-    origin = cv.imread(path)
+def detector(image):
+    origin = np.array(image)
     img = cv.resize(origin, (512, 512))
     data = np.array(img)
     data = np.reshape(data, (-1, 512, 512, 3))
@@ -18,6 +17,7 @@ def detector(path):
         saver = tf.train.Saver()
         saver.restore(sess, CKPT_PATH)
         pred = sess.run(model.y_, feed_dict={model.x: data})
+    tf.reset_default_graph()
     pred = np.argmax(pred, axis=-1)
     pred = np.reshape(pred, (128, 128))
     pred = pred.astype(np.uint8)
@@ -25,10 +25,15 @@ def detector(path):
 
     result = np.array(origin)
     result[:, :, 0] = origin[:, :, 0] + pred*24
+    return result
 
+from os import listdir
+cancer_path = "E:\SC\segmentation\cancer"
+import random
+imgs = listdir(cancer_path)
+for i in range(10):
+    img = cv.imread(cancer_path+'\\'+imgs[random.randint(0, 1000)])
+    result = detector(img)
     plt.rcParams['figure.dpi'] = 300
     plt.imshow(result)
-    plt.show()
-
-testpath = "E:\SC\segmentation\cancer\\00CBD45669DC06735ADC060D2C155C1E.tiff"
-detector(testpath)
+    plt.savefig("DemoPic\\"+str(i))
